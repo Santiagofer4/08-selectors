c37fa6901b1cce9224c4e23ad81140d179faa8ad
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-07-22 23:39:36 -0300

    section 265 finished

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
-import { Region } from '../../interfaces/country.interfaces';
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
 import { switchMap } from 'rxjs';
 
 @Component({
@@ -11,6 +11,8 @@ import { switchMap } from 'rxjs';
   ]
 })
 export class SelectorPageComponent implements OnInit {
+
+  public countriesByRegion: SmallCountry[] = []
 
   public myForm: FormGroup = this.fb.group({
     region: [ '', Validators.required ],
@@ -36,8 +38,8 @@ export class SelectorPageComponent implements OnInit {
       .pipe(
         switchMap( region => this.countriesService.getCountriesByRegion(region) )
       )
-      .subscribe( region => {
-        console.log('onRegionChanged',{region})
+      .subscribe( countries => {
+        this.countriesByRegion = countries
       })
   }
 
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country, Region, SmallCountry } from '../interfaces/country.interfaces';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -25,7 +25,12 @@ export class CountriesService {
     
     const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`
 
-    return this.http.get<SmallCountry[]>(url).pipe(
+    return this.http.get<Country[]>(url).pipe(
+      map( countries => countries.map( country =>  ({
+        name: country.name.common,
+        cca3: country.cca3,
+        borders: country.borders ?? []
+      }))),
       tap( resp => console.log(resp) )
     )
 
